24505ef421de1dae37f5079d3f53f9a56ba6982e
Дмитрий <tomountain@mail.ru>
2020-11-01 18:51:51 +0400

    fix: start app in gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { MainPage } from './components/mainPage';
 import 'antd/dist/antd.css';
 import './App.scss';
 
 const App: React.FunctionComponent = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <MainPage />
-        </Route>
-      </Switch>
-    </Router>
+    <MainPage />
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/">
+    //       <MainPage />
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
 };
 
